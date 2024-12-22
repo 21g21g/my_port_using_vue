fa212219d84acf8dc9216cd6c3fd34a98c87ba5d
21g21g <assegagebeyehu21@gmail.com>
2024-12-22 21:15:32 +0300

    this is my personal portifolio using vue js

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,42 @@
+<script setup>
+import image from "../assets/template.webp"
+import Card from 'primevue/card';
+const props=defineProps({
+    project:{
+      type:Object
+    }
+})
+</script>
+<template>
+  <div class="flex flex-col bg-white  mr-3 rounded-2xl   pb-10  relative  ring ring-transparent hover:ring-sky-500 group">
+    <Card style="overflow:visible ">
+      <template #header>
+        <img :src="props.project.photo" alt="there is no photo" class="object-cover h-[15rem] rounded-2xl" />
+      </template>
+      <template #title>
+        <h1 class="text-sky-500 py-2 text-2xl font-bold">{{ props.project.title }}</h1>
+      </template>
+      <template #content>
+        <div class="relative">
+          <p class="text-justify transition-all line-clamp-6 duration-300 text-[#334155] px-3  overflow-hidden text-sm">
+            {{ props.project.description }}
+          </p>
+          <!-- Fade-out effect -->
+          <!-- <div class="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-gray-900 to-transparent pointer-events-none"></div> -->
+        </div>
+      </template>
+    </Card>
+    <div class="absolute top-[28rem] left-56 md:left-72 transition-opacity duration-300">
+      <a 
+  :href="props.project?.link"
+  target="_blank" 
+  rel="noopener noreferrer" 
+  className="bg-sky-500 w-32 py-1 rounded-xl text-center block text-white hover:bg-sky-600 transition">
+  See More
+</a>
+    </div>
+  </div>
+</template>
+
+
+
---
+++ src/components/landing/Contacts.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from "vue";
+import Textarea from 'primevue/textarea';
+import InputText from 'primevue/inputtext';
+
+const firstName=ref("")
+const lastName=ref("")
+const phone=ref("")
+const email=ref("")
+const content=ref("")
+
+</script>
+<template>
+  <div class=" min-h-screen  bg-black w-full pt-24 pb-40">
+    <div class="flex flex-col px-16 md:px-64">
+      <h1 class="text-5xl text-white font-bold  md:self-center">Contact <span class="text-5xl text-teal-500">Me</span></h1>
+      <form class="mt-20 md:ml-24"  action="https://formsubmit.co/assegagebeyehu21@gmail.com"
+       method="POST">
+        <div class="flex flex-col gap-4">
+        <div class="flex flex-col md:flex-row w-[50rem] gap-4">
+       <InputText type="text" v-model="firstName" class="!bg-slate-600 !rounded-lg !py-3 !pl-2 !w-1/2 " placeholder="First Name" name="firstName"/>
+        <InputText type="text" v-model="lastName" class="!bg-slate-600 !rounded-lg !py-3 !pl-2 !w-1/2" placeholder="Last Name" name="lastName"/>
+
+      </div>
+       <div class="flex flex-col md:flex-row w-[50rem] gap-4">
+        <InputText type="number" v-model="phone" class="!bg-slate-600 !rounded-lg !pl-2 !py-3 !w-1/2" placeholder="Phone Number" name="phone"/>
+        <InputText type="email" v-model="email" class="!bg-slate-600 !rounded-lg !pl-2 !py-3 !w-1/2" placeholder="Email" name="email"/>
+
+      </div>
+      <Textarea v-model="content" rows="10"  class="!bg-slate-600 w-[25rem] md:!w-[50rem] !px-7 !pt-4 !rounded-lg" name="content"/>
+
+     </div>
+          <input type="hidden" name="_next" value="https://www.google.com/search?q=english+to+amharic+translation&oq=&gs_lcrp=EgZjaHJvbWUqBggBEEUYOzIGCAAQRRg5MgYIARBFGDsyDQgCEAAYgwEYsQMYgAQyDQgDEC4YgwEYsQMYgAQyDQgEEC4YxwEY0QMYgAQyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQgyNjY5ajBqNKgCALACAQ&sourceid=chrome&ie=UTF-8" />
+     
+    <button class="self-center bg-teal-500 hover:bg-blue-600 transition-all duration-700 rounded-xl mt-5 ml-10 md:ml-80 text-xl w-28 text-white px-4 py-1 font-bold" type="submit">Submit</button>
+
+      </form>
+    
+
+    </div>
+    
+    
+  </div>
+</template>
